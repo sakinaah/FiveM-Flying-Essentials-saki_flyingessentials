b2c0f7833b1fb1bd8cd831dc5f8ad4fe44ac5024
sakinaah <110434338+sakinaah@users.noreply.github.com>
2025-11-07 08:35:59 -0500

    Revise fxmanifest
    Updated the version and description, added server scripts.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'sakinah'
-description 'Removes turbulence while flying and disables the default GTA V game mechanic that shuts off planes when braking. Created by sakinaah.'
-version '1.0.0'
+description 'Flying improvements and essentials'
+version '1.0.3'
 
 client_scripts {
     'client.lua'
+}
+
+server_scripts {
+    'server.lua'
 }
